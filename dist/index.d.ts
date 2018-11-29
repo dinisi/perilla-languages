cdba6366032c4e8d36c75a10b880b47fef9e3517
ZhangZisu <admin@zhangzisu.cn>
2018-11-29 16:24:24 +0800

    Update

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+interface ILanguage {
+    name: string;
+    description: string;
+    syntaxPattern: string;
+}
+export declare const Languages: ILanguage[];
+export {};
---
+++ dist/index.js
@@ -0,0 +1,43 @@
+export const Languages = [
+    {
+        name: "c",
+        description: "C",
+        syntaxPattern: "c",
+    },
+    {
+        name: "cpp98",
+        description: "C++ 98",
+        syntaxPattern: "cpp",
+    },
+    {
+        name: "cpp11",
+        description: "C++ 11",
+        syntaxPattern: "cpp",
+    },
+    {
+        name: "java",
+        description: "Java",
+        syntaxPattern: "java",
+    },
+    {
+        name: "csharp",
+        description: "C#",
+        syntaxPattern: "csharp",
+    },
+    {
+        name: "python2",
+        description: "Python2",
+        syntaxPattern: "python",
+    },
+    {
+        name: "python3",
+        description: "Python3",
+        syntaxPattern: "python",
+    },
+    {
+        name: "node",
+        description: "Node.js",
+        syntaxPattern: "javascript",
+    },
+];
+//# sourceMappingURL=index.js.map
